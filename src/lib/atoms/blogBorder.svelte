082203dde00e10f1bfdbd4f82beb7132a8c6ce8b
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-11-23 23:43:15 +0100

    Cards & buttons

---
+++ src/lib/atoms/blogBorder.svelte
@@ -0,0 +1,36 @@
+<section>
+  <div class="cards">
+    <section class="cardyellow" />
+    <section class="cardscolordarkblue" />
+    <section class="cardyellow" />
+    <section class="cardscolordarkblue" />
+    <section class="cardyellow" />
+    <section class="cardscolordarkblue" />
+  </div>
+</section>
+
+<style>
+  .cardscolordarkblue {
+    width: 14em;
+    height: 20em;
+    border-radius: 2%;
+    border: 3px solid var(--vtDarkBlue);
+    padding: 0.6em;
+  }
+
+  .cardyellow {
+    width: 14em;
+    height: 20em;
+    border-radius: 2%;
+    border: 3px solid var(--vtYellow);
+    padding: 0.6em;
+  }
+
+  .cards {
+    display: grid;
+    grid-template-columns: 1fr 1fr 4fr;
+    justify-content: center;
+    align-items: center;
+    gap: 5em;
+  }
+</style>
---
+++ src/lib/atoms/blogButton.svelte
@@ -0,0 +1,20 @@
+<section>
+  <button>Lees meer</button>
+  <button>Lees meer</button>
+  <button>Lees meer</button>
+  <button>Lees meer</button>
+  <button>Lees meer</button>
+  <button>Lees meer</button>
+</section>
+
+<style>
+  button {
+    color: var(--vtDarkBlue);
+    border-radius: 0.3em;
+    background-color: var(--vtRed-50);
+    border: none;
+    height: 3em;
+    width: 7em;
+    margin-top: 1em;
+  }
+</style>
