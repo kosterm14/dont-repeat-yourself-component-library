959a70e06e5ed523f56dc8f48ac99e8eb9070ff4
Marwaxhello <marwahello33@gmail.com>
2023-11-29 22:44:13 +0100

    fetched data styling

--- src/lib/organisms/kennisclips.svelte
+++ src/lib/organisms/kennisclips.svelte
@@ -2,9 +2,48 @@
   export let data;
 </script>
 
-{#each data.categories as category}
+<!-- {#each data.categories as category}
   {category.title}
-  {category.content.html}
+  <p>{@html category.content.html}</p>
   {category.knowledgeClip.url}
+{/each} -->
+<main>
+
+{#each data.categories as category}
+
+  <article class="category">
+    <h2>{category.title}</h2>
+    <p>{@html category.content.html}</p>
+
+
+    <aside>
+    <button>
+    <img
+    class="left-arrow"
+    src="./arrow.svg"
+    alt="Knop met pijl naar volgende video"
+    />
+    </button>
+
+    <a href={category.knowledgeClip.url}>{category.knowledgeClip.url}</a>
+    
+    <button>
+    <img
+    class="right-arrow"
+    src="./arrow.svg"
+    alt="Knop met pijl naar volgende video"
+    />
+    </button>
+    </aside>
+
+  </article>
 {/each}
 
+</main>
+
+
+
+
+<style>
+
+</style>
