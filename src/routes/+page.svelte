522a3fd0bddd10acbe3b1d0d99a770f96ed10a97
Daniquedejong <danique.de.jong3@hva.nl>
2023-11-29 22:08:37 +0100

    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
-   import Header from "$lib/molecules/header.svelte";
+   import Footer from "$lib/molecules/footer.svelte";
+import Header from "$lib/molecules/header.svelte";
     import Homepage from "$lib/organisms/homepage.svelte";
 </script>
 
 <Header />
 <Homepage />
+<Footer />
 
  
