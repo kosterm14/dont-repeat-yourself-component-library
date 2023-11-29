b4dd409b3fcfedec9cd7004f13324642bbb4663c
Daniquedejong <danique.de.jong3@hva.nl>
2023-11-29 15:14:24 +0100

    Header en footer toegevoegd

---
+++ src/lib/molecules/footer.svelte
@@ -0,0 +1,91 @@
+<script>
+
+
+</script>
+
+<section>
+
+  <nav class="footer">
+
+  <img src="assets\visual-thinking-logo-footer.svg" alt="footer-logo">
+
+  <ul class="footer-menu">
+    <li>Over</li>
+    <li>Artikelen</li>
+    <li>Tekemethodes</li>
+    <li>Kennisclips</li>
+  </ul>
+
+  <ul class="social-media">
+  <h4>Contact</h4>
+  <img src="assets\social-media-icons\instagram.svg" alt="instagram">
+  <img src="assets\social-media-icons\youtube.svg" alt="youtube">
+  <img src="assets\social-media-icons\instagram.svg" alt="instagram">
+  <h5>© Charley Muhren, 2022</h5>
+  </ul>
+
+  </nav>
+  
+</section>
+
+<style>
+
+:root {
+    --vtPrimaryFont: "rigid-square", sans-serif;
+    --vtSecondaryFont: "yrsa", serif;
+}
+
+.footer {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); 
+    align-items: center;
+    gap: 10px; 
+    background-color: rgb(240, 240, 240);
+}
+
+li {
+    list-style-type: none;
+}
+
+img {
+    grid-column: 1;
+    margin-left: 3em;
+}
+
+.footer-menu {
+    grid-column: 2;
+    margin-left: 5em;
+}
+
+.social-media {
+    grid-column: 3;
+    
+}
+
+@media (max-width: 920px) { 
+.footer {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr); 
+    grid-template-rows: repeat(3, 1fr);
+    align-items: center;
+    gap: 10px;
+    margin-left: 1em;
+}
+
+img {
+    grid-column: 1;
+    grid-row: 1;
+}
+
+.footer-menu {
+    grid-column: 1;
+    grid-row: 2;
+}
+
+.social-media {
+    grid-column: 1;
+    grid-row: 3;
+}
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Header from "$lib/molecules/header.svelte";
+  import Tekenmethodes from "$lib/organisms/tekenmethodes.svelte";
+  import Footer from "$lib/molecules/footer.svelte";
+    export let data;
+</script>
+    
+    <Header />
+    <Tekenmethodes data={data}/>
+    <Footer />
+
+
+    
+    
