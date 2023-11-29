8d95926b1f12ba784c5572322a7c2ea2281d067c
Tobias Vrolijk <vrolijktobias@gmail.com>
2023-11-29 15:06:34 +0100

    Tekenruimte feedback toegepast

--- src/lib/organisms/tekenruimte.svelte
+++ src/lib/organisms/tekenruimte.svelte
@@ -1,6 +1,6 @@
 <script>
-    import Header from "../../lib/components/header.svelte";
-    import Footer from "../../lib/components/footer.svelte";
+    import Header from "$lib/molecules/header.svelte";
+    import Footer from "$lib/molecules/footer.svelte";
     import { onMount } from "svelte";
     onMount(() => {
         const canvas = document.getElementById("drawing-board");
@@ -81,33 +81,53 @@
     .labeltekst {
         font-family: var(--vtPrimaryFont);
         margin: auto 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
     .container {
       font-family: var(--vtPrimaryFont);  
       height: 100%;
       display: flex;
     }
+
+    .drawing-board{
+        border-radius: 25px;
+        border: 1.5px #090940 solid;
+        margin: 0.5em;
+    }
+
     #toolbar {
-        /* font-family: var(--vtPrimaryFont); */
         font-weight: 800;
         display: flex;
         flex-direction: column;
+        justify-content: center;
+        align-items: center;
         padding: 0.5em;
         width: 15%;
+        height: 50%;
         background-color: #feb51e;
-        border-bottom-right-radius: 25px;
-        border-top-right-radius: 25px;
+        border-radius: 25px;
         gap: 0.3em;
+        margin: 0.5em;
     }
+
     #toolbar input {
-        width: 100%;
+        width: 50%;
         border-radius: 4px;
+        margin: 0;
+    }
+
+    #toolbar input:active{
+        transform: translateY(1px);
+        
     }
 
     #stroke{
         border: none;
         background: none;
         border-radius: 4px;
+        
     }
 
     #lineWidth{
@@ -121,8 +141,24 @@
         color: white;
         padding: 2px;
         margin-top: 0.5em;
-        width: 100%;
+        margin-bottom: 0.5em;
+        width: 50%;
     }
+
+    #toolbar button:hover{
+        color: #feb51e;
+
+    }
+
+    #toolbar button:active{
+        transform: translateY(1px);
+
+    }
+
+    h3{
+        margin: 0.5em;
+    }
+
 
     @media screen and (max-width: 900px) {
       #toolbar{
