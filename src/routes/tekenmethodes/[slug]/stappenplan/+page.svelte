ea2ba987cea4d8f4c93176e3ee2d786269640411
Marco <marco.kosters@hva.nl>
2023-11-30 03:11:01 +0100

    Update +page.svelte

--- src/routes/tekenmethodes/[slug]/stappenplan/+page.svelte
+++ src/routes/tekenmethodes/[slug]/stappenplan/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     // import Header from "../../../../lib/components/header.svelte";
-    import Footer from "../../../../lib/components/footer.svelte";
+    // import Footer from "../../../../lib/components/footer.svelte";
 
     export let data;
     console.log(data);
